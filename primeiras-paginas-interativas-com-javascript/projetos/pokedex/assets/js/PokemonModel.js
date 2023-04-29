92430a88ddc43608600ec53ce117ab02146a6e85
lucasdev3 <lucasrepositoriodados@gmail.com>
2023-04-29 13:13:11 -0300

    update

--- primeiras-paginas-interativas-com-javascript/projetos/pokedex/assets/js/PokemonModel.js
+++ primeiras-paginas-interativas-com-javascript/projetos/pokedex/assets/js/PokemonModel.js
@@ -1,5 +1,6 @@
 // Define a classe Pokemon e o método render
 class Pokemon {
+  
     constructor(id, name, types) {
       this.id = id;
       this.name = name;
@@ -9,21 +10,25 @@ class Pokemon {
   
     render() {
       const listTypesHtml = this.types.length > 0
-        ? this.types.map((type) => `<li class="type">${type.type.name}</li>`).join('')
-        : `<li class="type">None</li>`;
+        ? this.types.map((type) => `<li class="${type.type.name}">${type.type.name}</li>`).join('')
+        : `<li class="${type.type.name}">None</li>`;
+    
+      // Obter o nome do primeiro tipo (caso exista)
+      const type = this.types.length > 0 ? this.types[0].type.name : '';
+    
       return `
-        <li class="pokemon">
-            <span class="number">${this.id}</span>
-            <span class="name">${this.name}</span>
-  
-            <div class="detail">
-                <ol class="types">
-                    ${listTypesHtml}
-                </ol>
-                <img src="${this.urlImage}">
-                
-            </div>
+        <li class="pokemon ${type}">
+          <span class="number">${this.id}</span>
+          <span class="name">${this.name}</span>
+    
+          <div class="detail">
+            <ol class="types">
+              ${listTypesHtml}
+            </ol>
+            <img src="${this.urlImage}">
+          </div>
         </li>
       `;
     }
+
   }
